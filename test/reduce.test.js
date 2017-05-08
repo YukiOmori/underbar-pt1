750c0de59a90c88e7527b91a1f5e9fd1cdf9aa72
Yuki Omori <yuki.omori@inte.co.jp>
2017-05-08 17:49:25 +0900

    added test to reduce

--- test/reduce.test.js
+++ test/reduce.test.js
@@ -7,10 +7,18 @@ describe('reduce()', () => {
     expect(result).toEqual(15);
   });
 
+  it('reduces an array of numbers to a sum, *with* an explicit initial value for the accumulator', () => {
+    const nums = ['a', 'b', 'c', 'd', 'e'];
+    const result = _.reduce(nums, (acc, num) => acc + num, 'x');
+    expect(result).toEqual('xabcde');
+  });
+
   it('reduces an array of numbers to a sum, *without* an explicit initial value for the accumulator', () => {
     const nums = [1, 2, 3, 4, 5];
     const result = _.reduce(nums, (a, b) => a + b);
     expect(result).toEqual(15);
   });
+
+
 
 });
