8dcf4183668541f6904e125d98d731c471be3a22
Yuki Omori <yuki.omori@inte.co.jp>
2017-05-08 17:37:02 +0900

    added test to map

--- test/map.test.js
+++ test/map.test.js
@@ -6,4 +6,16 @@ describe('map()', () => {
     const mappedArr = _.map(arr, (el) => el * el);
     expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
   });
+
+  it('maps every numbers in an array of numbers to their square', () => {
+    const obj = {
+      1: 'a',
+      2: 'b',
+      3: 'c',
+      4: 'd',
+      5: 'e'
+    };
+    const mappedArr = _.map(obj, (el) => el + el);
+    expect(mappedArr).toEqual(['aa', 'bb', 'cc', 'dd', 'ee']);
+  });
 });
