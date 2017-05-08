91f741e288684ae41174677c720915f0662bd859
Yuki Omori <yuki.omori@inte.co.jp>
2017-05-08 18:30:38 +0900

    added test to some

--- test/some.test.js
+++ test/some.test.js
@@ -1,13 +1,33 @@
 const _ = require('../underbar');
 
 describe('some()', () => {
-  it('returns true if any number is odd', () => {
+  it('returns true if any number of an array is odd', () => {
     const nums = [2, 4, 5, 6];
     expect(_.some(nums, num => num % 2 === 1)).toBe(true);
   });
 
-  it('returns false if no number is odd', () => {
+  it('returns false if no number of an array is odd', () => {
     const nums = [2, 4, 6, 8];
+    expect(_.some(nums, num => num % 2 === 1)).toBe(false);
+  });
+
+  it('returns true if any number of an object is odd', () => {
+    const nums = {
+      0: 2,
+      1: 4,
+      2: 5,
+      3: 6
+    };
+    expect(_.some(nums, num => num % 2 === 1)).toBe(true);
+  });
+
+  it('returns false if no number of an object is odd', () => {
+    const nums = {
+      0: 2,
+      1: 4,
+      2: 6,
+      3: 8
+    };
     expect(_.some(nums, num => num % 2 === 1)).toBe(false);
   });
 
